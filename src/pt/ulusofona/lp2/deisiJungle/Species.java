8b2dcb43cd2b7fa9f15a1c2930f8c4a966f8d815
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2022-11-02 00:17:19 +0000

    Implements 'Species' class

---
+++ src/pt/ulusofona/lp2/deisiJungle/Species.java
@@ -0,0 +1,13 @@
+package pt.ulusofona.lp2.deisiJungle;
+
+public class Species {
+    Character id;
+    String name;
+    String imageFilename;
+
+    public Species(Character id, String name, String imageFilename) {
+        this.id = id;
+        this.name = name;
+        this.imageFilename = imageFilename;
+    }
+}
