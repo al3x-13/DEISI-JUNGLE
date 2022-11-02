c4de5723aea3cdc6d1a20b368f45d20d53958551
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2022-11-02 13:26:23 +0000

    Initializes 'GameMap' with the proper size

--- src/pt/ulusofona/lp2/deisiJungle/GameMap.java
+++ src/pt/ulusofona/lp2/deisiJungle/GameMap.java
@@ -1,5 +1,24 @@
 package pt.ulusofona.lp2.deisiJungle;
 
 public class GameMap {
+    int size;
     MapCell[] map;
+
+    GameMap(int size) {
+        if (size < 1) {
+            throw new IllegalArgumentException("Map size must be greater than 1!");
+        }
+
+        this.size = size;
+        this.map = new MapCell[size];
+
+        // Initializes each cell of the map to its corresponding type
+        for (int i = 0; i < this.map.length; i++) {
+            if (i == this.map.length - 1) {
+                this.map[i] = new MapCell("Meta");
+            } else {
+                this.map[i] = new MapCell("Vazio");
+            }
+        }
+    }
 }
