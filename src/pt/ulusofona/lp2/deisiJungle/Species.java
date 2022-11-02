c9eae65f578ffe884c58c44c1e38cd95156798fb
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2022-11-02 01:13:37 +0000

    Adds 'getSpeciesData'

--- src/pt/ulusofona/lp2/deisiJungle/Species.java
+++ src/pt/ulusofona/lp2/deisiJungle/Species.java
@@ -10,4 +10,15 @@ public class Species {
         this.name = name;
         this.imageFilename = imageFilename;
     }
+
+    /**
+     * Provides information about the species. The format is a String array as follows:<p>
+     * [0] -> Species ID<p>
+     * [1] -> Species Name<p>
+     * [2] -> Species Image Filename, e.g. 'lion.png'
+     * @return Species data
+     */
+    String[] getSpeciesData() {
+        return new String[] { this.id.toString(), this.name, this.imageFilename };
+    }
 }
