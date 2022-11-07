1f645af603b310bd758de6778416ab9dbbac610e
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2022-11-07 15:54:54 +0000

    Implements 'getFinishMapCellIndex', 'movePlayer' and 'placePlayerInCell' functions

--- src/pt/ulusofona/lp2/deisiJungle/GameMap.java
+++ src/pt/ulusofona/lp2/deisiJungle/GameMap.java
@@ -31,6 +31,13 @@ public class GameMap {
     }
 
     /**
+     * @return Index (game index) of the MapCell with finish
+     */
+    int getFinishMapCellIndex() {
+        return this.size - 1;
+    }
+
+    /**
      * Gets the corresponding Cell for the given index
      * @param squareNr Cell index
      * @return Cell
@@ -42,5 +49,45 @@ public class GameMap {
             }
         }
         return null;
+    }
+
+    /**
+     * Moves the given player from its current position to the given destination.
+     * @param player Player to move
+     * @param destIndex Index of the destination MapCell
+     * @return Whether the player was successfully moved to the desired destination or not
+     */
+    boolean movePlayer(Player player, int destIndex, int energyCost) {
+        // Gets player current position in the map
+        int currentIndex = player.getCurrentMapPosition();
+
+        // Removes player from current 'MapCell'
+        if (!this.map[currentIndex - 1].rmPlayer(player.getID())) {
+            return false;
+        }
+
+        // Adds player to destination 'MapCell'
+        if (!this.map[destIndex - 1].addPlayer(player.getID())) {
+            return false;
+        }
+
+        // Charge energy for the player move
+        player.decreaseEnergy(2);
+
+        // Updates player current map position
+        player.updateMapPosition(destIndex);
+
+        return true;
+    }
+
+    /**
+     * Places a player in a cell without any previous verification or validation.
+     * Does not remove the player from the previous cell it was in, it that is the case.
+     * @param playerID Player ID
+     * @param cellIndex Destination cell index
+     * @return Whether the player was successfully placed in the destination cell
+     */
+    boolean placePlayerInCell(int playerID, int cellIndex) {
+        return this.map[cellIndex - 1].addPlayer(playerID);
     }
 }
