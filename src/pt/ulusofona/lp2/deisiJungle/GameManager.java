090a85702a34da781a8ed034da81bd0d92a93190
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2022-11-09 18:17:12 +0000

    Fixes another bug introduced in the latest commit (LULz)

--- src/pt/ulusofona/lp2/deisiJungle/GameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/GameManager.java
@@ -322,7 +322,7 @@ public class GameManager {
                     gameResults.add(
                             new StringBuilder()
                                     .append("#")
-                                    .append(i)
+                                    .append(i + 1)
                                     .append(" ")
                                     .append(player.getName())
                                     .append(", ")
