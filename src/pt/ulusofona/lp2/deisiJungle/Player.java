7458233b1a840604b3ef33debe742997f9b21422
C0NN3CT10N-L0ST <alexandreromba@hotmail.com>
2022-12-27 04:29:06 +0000

    Adds more relevant attributes to 'Player' as well as 'setEnergy' method

--- src/pt/ulusofona/lp2/deisiJungle/Player.java
+++ src/pt/ulusofona/lp2/deisiJungle/Player.java
@@ -1,17 +1,19 @@
 package pt.ulusofona.lp2.deisiJungle;
 
 public class Player {
-    int id;
-    String name;
-    Species species;
-    int energyUnits;
-    int currentMapPosition = 1;  // Game cells' index starts at 1
+    private int id;
+    private String name;
+    private Species species;
+    private int energyUnits;
+    private int currentMapPosition = 1;  // Game cells' index starts at 1
+    private int distanceCovered = 0;
+    private int consumedBananas = 0;
 
-    public Player(int id, String name, Species species, int energyUnits) {
+    public Player(int id, String name, Species species) {
         this.id = id;
         this.name = name;
         this.species = species;
-        this.energyUnits = energyUnits;
+        this.energyUnits = species.getEnergyUnits();
     }
 
     /**
@@ -33,6 +35,14 @@ public class Player {
      */
     int getEnergy() {
         return this.energyUnits;
+    }
+
+    /**
+     * Sets player energy to the given value.
+     * @param energy New player energy
+     */
+    void setEnergy(int energy) {
+        this.energyUnits = energy;
     }
 
     /**
